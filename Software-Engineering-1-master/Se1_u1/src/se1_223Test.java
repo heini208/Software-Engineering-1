45ee6b69f0c66efe12a8252f82b440ae1432da13
Dechey <57325926+Dechey@users.noreply.github.com>
2020-05-03 22:13:40 +0200

    Add files via upload

---
+++ Software-Engineering-1-master/Se1_u1/src/se1_223Test.java
@@ -0,0 +1,34 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class se1_223Test {
+	se1_223 obj0;
+	se1_223 obj1;
+	String[][] depend0 = {{ "A" , "C" }, { "C", "D" }, { "B", "C" }}; //transitivitaet
+	String[][] depend1 = null;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		obj0 = new se1_223Entwicklung(depend0);
+		obj1 = new se1_223Entwicklung(depend1);
+	}
+	
+	@DisplayName("Überprüft ob Sequence korrekt ist")
+	@Test
+	void testIsWellSorted() {
+		String[] seq0 = {"A", "B", "C", "D"};
+		String[] seq1 = {"C", "B", "A", "D"};
+		String[] seq2 = {"A", "B", "A", "C"};
+		String[] seq3 = {"A", "C", "B", "D"};
+		assertEquals(true,obj0.isWellSoreted(seq0));
+		assertEquals(false,obj0.isWellSoreted(seq1));
+		assertEquals(false,obj0.isWellSoreted(seq2));
+		assertEquals(false,obj0.isWellSoreted(seq3));
+		assertEquals(true,obj1.isWellSoreted(seq0));
+	}
+
+}
---
+++ Software-Engineering-1-master/Se1_u1/src/theorycrafting.java
@@ -0,0 +1,25 @@
+
+public class theorycrafting {
+	
+	gelsen
+	// hier noch ist Abhäng == null , wenn ja return true wenn nein mache nichts
+	for  j: {
+		//hier noch ist sequence von j in gelesen? wenn ja return false wenn nein mach nichts
+		for  i :{ 
+		if( Abhäng[i][1] == sequence [j]){
+			for k () {
+			if (Abhäng [i][0] == gelesen [k]){
+		 	
+			break;
+				}else {
+				return false;
+				}
+			}
+			}
+		}
+		gelesen[j] = sequence[j];
+	}
+	return true;
+}
+
+
