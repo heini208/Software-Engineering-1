265cf6cef0e34f21fec1f2a71587a015184e256a
nge-rez <57622090+nge-rez@users.noreply.github.com>
2020-05-03 21:31:10 +0100

    addedTests

--- Se1_u1/src/se1_223Test.java
+++ Se1_u1/src/se1_223Test.java
@@ -7,23 +7,28 @@ import org.junit.jupiter.api.Test;
 
 class se1_223Test {
 	se1_223 obj0;
-	String[][] depend = {{ "A" , "C" }, { "C", "D" }, { "B", "C" }};
+	se1_223 obj1;
+	String[][] depend0 = {{ "A" , "C" }, { "C", "D" }, { "B", "C" }}; //transitivitaet
+	String[][] depend1 = null;
 	
 	@BeforeEach
 	void setUp() throws Exception {
-		obj0 = new Sort(depend);
+		obj0 = new Sort(depend0);
+		obj1 = new Sort(depend1);
 	}
-
-	@AfterEach
-	void tearDown() throws Exception {
-	}
-
 	
-	@DisplayName("Überprügt ob Sequence korrekt sit")
+	@DisplayName("Überprüft ob Sequence korrekt ist")
 	@Test
 	void testIsWellSorted() {
-		String[] seq = {"A", "B", "C", "D"};
-		assertEquals(true,obj0.isWellSoreted(seq));
+		String[] seq0 = {"A", "B", "C", "D"};
+		String[] seq1 = {"C", "B", "A", "D"};
+		String[] seq2 = {"A", "B", "A", "C"};
+		String[] seq3 = {"A", "C", "B", "D"};
+		assertEquals(true,obj0.isWellSoreted(seq0));
+		assertEquals(false,obj0.isWellSoreted(seq1));
+		assertEquals(false,obj0.isWellSoreted(seq2));
+		assertEquals(false,obj0.isWellSoreted(seq3));
+		assertEquals(true,obj1.isWellSoreted(seq0));
 	}
 
 }
