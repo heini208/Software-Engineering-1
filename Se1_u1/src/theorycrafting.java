903462e868adaa2b20c9d2dadd22d01c04ae394d
heini208 <57675018+heini208@users.noreply.github.com>
2020-05-03 21:26:10 +0200

    Update theorycrafting.java

--- Se1_u1/src/theorycrafting.java
+++ Se1_u1/src/theorycrafting.java
@@ -2,6 +2,7 @@
 public class theorycrafting {
 	
 	gelsen
+	// hier noch ist Abhäng == null , wenn ja return true wenn nein mache nichts
 	for  j: {
 		//hier noch ist sequence von j in gelesen? wenn ja return false wenn nein mach nichts
 		for  i :{ 
