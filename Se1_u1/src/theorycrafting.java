8f2204188aee9545c9ccae5cda0cd9c5eb5c62d4
heini208 <57675018+heini208@users.noreply.github.com>
2020-05-03 21:06:54 +0200

    Update theorycrafting.java

--- Se1_u1/src/theorycrafting.java
+++ Se1_u1/src/theorycrafting.java
@@ -3,6 +3,7 @@ public class theorycrafting {
 	
 	gelsen
 	for  j: {
+		//hier noch ist sequence von j in gelesen? wenn ja return false wenn nein mach nichts
 		for  i :{ 
 		if( Abhäng[i][1] == sequence [j]){
 			for k () {
