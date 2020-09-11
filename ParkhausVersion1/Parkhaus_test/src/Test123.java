da68937815d8bf25408bb57a528adcc478da0058
heini208 <57675018+heini208@users.noreply.github.com>
2020-09-11 15:41:19 +0200

    test2

---
+++ ParkhausVersion1/Parkhaus_test/src/Test123.java
@@ -0,0 +1,16 @@
+
+public class Test123 {
+ int test;
+ public int testertest() {
+	 test++;
+	 test/=test;
+	 if (test ==1) {
+		 test-=1;
+	 }
+	 test = 123;
+	 test = 1234;
+	 test = 123;
+	 test = 123;
+	 return test;
+ }
+}
