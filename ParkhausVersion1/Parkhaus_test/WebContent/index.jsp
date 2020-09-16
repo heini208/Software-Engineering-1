4648c14a9d3f0dd027c3837e1c43f728a9e6aa7d
Dechey <57325926+Dechey@users.noreply.github.com>
2020-09-16 09:10:48 +0200

    PieChartBuilderTest | Custom Images

--- ParkhausVersion1/Parkhaus_test/WebContent/index.jsp
+++ ParkhausVersion1/Parkhaus_test/WebContent/index.jsp
@@ -32,6 +32,11 @@
     "Familie": 0.8,
     "Behinderung": 0.5}'
     
+     images='{
+     "car": "https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/68966/retro-car-icon-clipart-md.png",
+     "parking_garage": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTJh_9Wk7eMUnTMcD_n-pDUX8OWM_qNwL46PA&usqp=CAU",
+    "empty": "https://cdn.iconscout.com/icon/free/png-512/road-327-449812.png"
+    }'
   
  	
  >
---
+++ ParkhausVersion1/Parkhaus_test/src/PieChartBuilderTest.java
@@ -0,0 +1,30 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PieChartBuilderTest {
+
+	
+	
+	
+//	Erster Test auf Formatierung 
+	
+	
+	@Test
+	void test() {
+		int[] values = {10,2,6,5};
+		
+		String[] labels = {"Any","Frau","Behinderung","Familie"};
+		
+		String expectedString = 
+				"{\"data\":[{\"values\":[10,2,6,5],"
+				+ "\"type\":\"pie\","
+				+ "\"labels\":[\"Any\",\"Frau\",\"Behinderung\",\"Familie\"]}]}";
+				
+		String resultString = PieChartBuilder.BuildPieChart(labels,values);
+		
+		assertEquals(expectedString, resultString);
+	}
+}
+
+
